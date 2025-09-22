f2caa5372536510fba5837d78c1546df76a4fc25
dev-lv-megahoot <devlv@megahoot.com>
2025-09-22 15:18:16 +0530

    setup init

---
+++ target/debug/deps/pecunovus-2c4bcb9047be760d.d
@@ -0,0 +1,18 @@
+/Users/aakashbhardwaj/megahoot/pecunovus/target/debug/deps/pecunovus-2c4bcb9047be760d.d: src/lib.rs src/node/mod.rs src/network/mod.rs src/consensus/mod.rs src/ledger/mod.rs src/runtime/mod.rs src/state/mod.rs src/txpool/mod.rs src/storage/mod.rs src/crypto/mod.rs src/utils/mod.rs src/rpc/mod.rs
+
+/Users/aakashbhardwaj/megahoot/pecunovus/target/debug/deps/libpecunovus-2c4bcb9047be760d.rlib: src/lib.rs src/node/mod.rs src/network/mod.rs src/consensus/mod.rs src/ledger/mod.rs src/runtime/mod.rs src/state/mod.rs src/txpool/mod.rs src/storage/mod.rs src/crypto/mod.rs src/utils/mod.rs src/rpc/mod.rs
+
+/Users/aakashbhardwaj/megahoot/pecunovus/target/debug/deps/libpecunovus-2c4bcb9047be760d.rmeta: src/lib.rs src/node/mod.rs src/network/mod.rs src/consensus/mod.rs src/ledger/mod.rs src/runtime/mod.rs src/state/mod.rs src/txpool/mod.rs src/storage/mod.rs src/crypto/mod.rs src/utils/mod.rs src/rpc/mod.rs
+
+src/lib.rs:
+src/node/mod.rs:
+src/network/mod.rs:
+src/consensus/mod.rs:
+src/ledger/mod.rs:
+src/runtime/mod.rs:
+src/state/mod.rs:
+src/txpool/mod.rs:
+src/storage/mod.rs:
+src/crypto/mod.rs:
+src/utils/mod.rs:
+src/rpc/mod.rs:
